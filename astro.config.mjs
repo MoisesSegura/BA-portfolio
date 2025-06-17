9d3ba127472401a8e0bba287ae23054a3c22f69f
MoisesSegura <moisessegurarojas@gmail.com>
2025-06-16 19:18:11 -0600

    Fix trailingSlash issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://moisessegura.github.io',
-   base: '/BA-portfolio/',
+  base: '/BA-portfolio/',
+  trailingSlash: 'always',
   
   vite: {
     plugins: [tailwindcss()]
